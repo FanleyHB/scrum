ee50eec25a384a2a14a4c0543965505fc7ea5d67
FanleyHB <fanley@outlook.fr>
2019-01-16 16:29:08 +0100

    Intégration des données de la base de données dans la page

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+use App\Entity\Recette;
+use App\EntityManager\RecetteManager;
+
+require 'src/Lib/autoload.php';
+require 'src/Lib/config.php';
+require 'src/Lib/pdo.php';
+
+$em = new RecetteManager();
+
+$recette = $em->getRecetteDuJour();
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -63,7 +74,7 @@
             </div>
 
             <!-- Title -->
-            <h1 class="mt-4 text-success">Recette du jour</h1>
+            <h1 class="mt-4 text-success">Recette du jour : <?php echo $recette->getTitre(); ?></h1>
 
             <hr>
 
@@ -75,23 +86,23 @@
                 <div class="row p-0 m-0 text-center d-flex justify-content-around">
                     <div class="col-md-2 border m-0 ">
                         Temps de préparation <br>
-                        {Temps}
+                        <span class="text-success"><?php echo $recette->getTemps(); ?>mn</span>
                     </div>
                     <div class="col-md-2 border m-0 ">
                         Nombres de personnes <br>
-                        {Personnes}
+                        <span class="text-success"><?php echo $recette->getPourCombien(); ?></span>
                     </div>
                     <div class="col-md-2 border m-0 ">
                         Difficulté <br>
-                        {Niveau de difficulté}
+                        <span class="text-success"><?php echo $recette->getDifficulte(); ?></span>
                     </div>
                     <div class="col-md-2 border m-0 ">
                         Prix <br>
-                        {€}
+                        <span class="text-success"><?php echo $recette->getPrix(); ?></span>
                     </div>
                     <div class="col-md-2 border m-0 ">
                         Coût Carbone <br>
-                        {CO}
+                        <span class="text-success"><?php echo $recette->getEmpreinteCarbone(); ?></span>
                     </div>
                 </div>
             </div>
@@ -99,32 +110,7 @@
             <hr>
 
             <!-- Post Content -->
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut,
-                error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae
-                laborum minus inventore?</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste
-                ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus,
-                voluptatibus.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde
-                eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis.
-                Enim, iure!</p>
-
-            <blockquote class="blockquote">
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a
-                    ante.</p>
-                <footer class="blockquote-footer">Someone famous in
-                    <cite title="Source Title">Source Title</cite>
-                </footer>
-            </blockquote>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat
-                totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam
-                tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae?
-                Qui, necessitatibus, est!</p>
+            <?php echo $recette->getDescription(); ?>
 
             <hr>
 
